70d6297b1a441409c4f4632bc10a8686c4fe4d2c
Simon Lendvai <simon.lendvai@elev.ga.ntig.se>
2026-04-16 15:24:16 +0200

    savemyshidawg

--- app.rb
+++ app.rb
@@ -5,11 +5,14 @@ require 'sinatra/reloader'
 require 'bcrypt'
 
 enable :sessions
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 
 get('/') do
   db = SQLite3::Database.new("db/posts.db")
   id = params[:id].to_i
   db.results_as_hash = true
+  db.execute("PRAGMA encoding = 'UTF-8'")
   @posts = db.execute("SELECT * FROM posts")
   slim(:home)
 end
@@ -21,6 +24,7 @@ post('/upload') do
   title = params[:title]
   description = params[:description]
   price = params[:price].to_f
+  price_type = params[:price_type]
 
   if params[:image] && params[:image][:tempfile] && params[:image][:filename]
     tempfile = params[:image][:tempfile]
@@ -37,15 +41,20 @@ post('/upload') do
   end
 
   db.execute(
-    "INSERT INTO posts (title, description, price, image_url) VALUES (?, ?, ?, ?)",
-    [title, description, price, image_url]
+  "INSERT INTO posts (title, description, price, price_type, image_url, user_id)
+   VALUES (?, ?, ?, ?, ?, ?)",
+  [title, description, price, price_type, image_url, session[:user_id]]
   )
 
   redirect('/')
 end
 
 get('/upload') do
-  slim(:upload)
+  if session[:user_id]
+    slim(:upload)
+  else
+    redirect('/login')
+  end
 end
 
 post("/:id/return") do
@@ -58,4 +67,62 @@ get('/:id/post') do
   db.results_as_hash = true
   @posts = db.execute("SELECT * FROM posts WHERE id = ?", id).first
   slim(:post)
+end
+
+get('/register') do
+  slim(:register)
+end
+
+post('/register') do
+  db = SQLite3::Database.new("db/users.db")
+
+  username = params[:username]
+  password = params[:password]
+
+  password_digest = BCrypt::Password.create(password)
+
+  db.execute(
+    "INSERT INTO users (username, password_digest) VALUES (?, ?)",
+    [username, password_digest]
+  )
+
+  redirect('/login')
+end
+
+get('/login') do
+  slim(:login)
+end
+
+post('/login') do
+  db = SQLite3::Database.new("db/users.db")
+  db.results_as_hash = true
+
+  username = params[:username]
+  password = params[:password]
+
+  user = db.execute("SELECT * FROM users WHERE username = ?", [username]).first
+
+  if user && BCrypt::Password.new(user["password_digest"]) == password
+    session[:user_id] = user["id"]
+    redirect('/')
+  else
+    @error = "Fel användarnamn eller lösenord"
+    slim(:login)
+  end
+end
+
+get('/logout') do
+  session.clear
+  redirect('/')
+end
+
+get('/myads') do
+  if session[:user_id]
+    db = SQLite3::Database.new("db/posts.db")
+    db.results_as_hash = true
+    @posts = db.execute("SELECT * FROM posts WHERE user_id = ?", session[:user_id])
+    slim(:myads)
+  else
+    redirect('/login')
+  end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -1,9 +1,20 @@
 require 'sqlite3'
 
-db = SQLite3::Database.new("posts.db")
+db1 = SQLite3::Database.new("posts.db")
+
+db2 = SQLite3::Database.new("users.db")
 
 def seed!(db)
   puts "Using db file: posts.db"
+  puts "🧹 Dropping old tables..."
+  drop_tables(db)
+  puts "🧱 Creating tables..."
+  create_tables(db)
+  puts "🍎 Populating tables..."
+  populate_tables(db)
+  puts "✅ Done seeding the database!"
+
+  puts "Using db file: users.db"
   puts "🧹 Dropping old tables..."
   drop_tables(db)
   puts "🧱 Creating tables..."
@@ -15,6 +26,7 @@ end
 
 def drop_tables(db)
   db.execute('DROP TABLE IF EXISTS posts')
+  db.execute('DROP TABLE IF EXISTS users')
 end
 
 def create_tables(db)
@@ -22,15 +34,25 @@ def create_tables(db)
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               title TEXT NOT NULL, 
               description TEXT,
-              price_period TEXT,
+              price_type TEXT,
               price INTEGER,
-              image_url TEXT)')
+              image_url TEXT,
+              user_id INTEGER
+              )')
+  db.execute('CREATE TABLE users (
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              username TEXT UNIQUE NOT NULL,
+              password_digest TEXT NOT NULL
+              )')
 end
 
 def populate_tables(db)
-  db.execute('INSERT INTO posts (title, description, image_url) VALUES ("Goyslop", "säljer denna goyslop bilig", "/img/blowie.jpg")')
-  db.execute('INSERT INTO posts (title, description, image_url) VALUES ("mer goy zlob", "goyslop", "/img/DAMN!!!!!!!!!!!!.jpg")')
-  db.execute('INSERT INTO posts (title, description, image_url) VALUES ("a a hahuh", "ahahahgags", "/img/green_fuck.jpg")')
+  db.execute('INSERT INTO posts (title, description, price, price_type, image_url, user_id) VALUES("Betongbarriär", "Betongbarriär uthyres. Kan användas för avskärmning av större ytor utomhus, osv. Pris: 50 kr/dag", 50, "hour", "/img/barriar.jpg", 1)')
+  db.execute('INSERT INTO posts (title, description, price, price_type, image_url, user_id) VALUES("mer goy zlob", "goyslop", 100, "day", "/img/DAMN!!!!!!!!!!!!.jpg", 2)')
+  db.execute('INSERT INTO posts (title, description, price, price_type, image_url, user_id) VALUES("a a hahuh", "ahahahgags", 200, "week", "/img/green_fuck.jpg", 1)')
+  db.execute('INSERT INTO users (username, password_digest) VALUES ("john_doe", "fella")')
+  db.execute('INSERT INTO users (username, password_digest) VALUES ("jane_doe", "password123")')
 end
 
-seed!(db)
+seed!(db1)
+seed!(db2)
