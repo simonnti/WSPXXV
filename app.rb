e624c89c5ad93d0422896fa64031e27503a37692
Simon Lendvai <simon.lendvai@elev.ga.ntig.se>
2026-04-16 16:08:36 +0200

    crudfix?

--- app.rb
+++ app.rb
@@ -5,14 +5,11 @@ require 'sinatra/reloader'
 require 'bcrypt'
 
 enable :sessions
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
 
 get('/') do
   db = SQLite3::Database.new("db/posts.db")
   id = params[:id].to_i
   db.results_as_hash = true
-  db.execute("PRAGMA encoding = 'UTF-8'")
   @posts = db.execute("SELECT * FROM posts")
   slim(:home)
 end
@@ -28,9 +25,13 @@ post('/upload') do
 
   if params[:image] && params[:image][:tempfile] && params[:image][:filename]
     tempfile = params[:image][:tempfile]
+
     filename = params[:image][:filename]
+    filename = filename.force_encoding("UTF-8")
+    filename = filename.gsub(/[^0-9A-Za-z.\-]/, "_")
 
     save_path = "./public/img/#{filename}"
+
     File.open(save_path, "wb") do |f|
       f.write(tempfile.read)
     end
@@ -41,9 +42,9 @@ post('/upload') do
   end
 
   db.execute(
-  "INSERT INTO posts (title, description, price, price_type, image_url, user_id)
-   VALUES (?, ?, ?, ?, ?, ?)",
-  [title, description, price, price_type, image_url, session[:user_id]]
+    "INSERT INTO posts (title, description, price_type, price, image_url, user_id)
+     VALUES (?, ?, ?, ?, ?, ?)",
+    [title, description, price_type, price, image_url, session[:user_id]]
   )
 
   redirect('/')
@@ -124,5 +125,62 @@ get('/myads') do
     slim(:myads)
   else
     redirect('/login')
+  end
+end
+
+post('/:id/update') do
+  if session[:user_id]
+    db = SQLite3::Database.new("db/posts.db")
+    id = params[:id].to_i
+    db.results_as_hash = true
+    post = db.execute("SELECT * FROM posts WHERE id = ?", [id]).first
+
+    if post && post["user_id"] == session[:user_id]
+      title = params[:title]
+      description = params[:description]
+      price = params[:price].to_f
+      price_type = params[:price_type]
+
+      if params[:image] && params[:image][:tempfile] && params[:image][:filename]
+        tempfile = params[:image][:tempfile]
+
+        filename = params[:image][:filename]
+        filename = filename.force_encoding("UTF-8")
+        filename = filename.gsub(/[^0-9A-Za-z.\-]/, "_")
+
+        save_path = "./public/img/#{filename}"
+
+        File.open(save_path, "wb") do |f|
+          f.write(tempfile.read)
+        end
+
+        image_url = "/img/#{filename}"
+      else
+        image_url = post["image_url"]
+      end
+
+      db.execute(
+        "UPDATE posts SET title = ?, description = ?, price_type = ?, price = ?, image_url = ? WHERE id = ?",
+        [title, description, price_type, price, image_url, id]
+      )
+    end
+  end
+
+  redirect('/myads')
+end
+
+get('/:id/update') do
+  redirect('/login') unless session[:user_id]
+
+  db = SQLite3::Database.new("db/posts.db")
+  db.results_as_hash = true
+
+  id = params[:id].to_i
+  @posts = db.execute("SELECT * FROM posts WHERE id = ?", [id]).first
+
+  if @posts && @posts["user_id"] == session[:user_id]
+    slim(:update)
+  else
+    redirect('/myads')
   end
 end
